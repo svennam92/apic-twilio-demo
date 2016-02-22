bfda07818273e2f6550b8ef6a61dd8ddb91e4d2d
Sai Vennam <svennam@us.ibm.com>
2016-02-22 15:28:03 -0800

    add twilio support

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-
 var express = require('express');
+var fs = require('fs');
+var util = require('util');
+var qs = require('querystring');
+var prettyjson = require('prettyjson');
 var twilio = require('twilio');
 var client = twilio('AC6e318d67d6fd4d680861e8bdae90332a', 'e008985209c7906ef710085f2169280f');
 var Client = require('node-rest-client').Client;
- 
+
 var client = new Client();
 /*
 var mysql      = require('mysql');
@@ -15,30 +18,53 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 */
+
+
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
-  try{
-  getEmployeesFromAPI("Berni");
-}catch(err){
-	console.log("ERROR: " , err);
-}
+  //  try {
+  //    getEmployeesFromAPI("Berni", res);
+  //  } catch (err) {
+  //    console.log("ERROR: ", err);
+  //  }
 
 });
 
 app.get('/:firstName', function (req, res) {
- // res.send('Hello World!');
+  // res.send('Hello World!');
   getEmployeesFromAPI(req.params.firstName, res);
 
 });
 
-app.post('/', function (req,res) {
-  var twiml = new twilio.TwimlResponse();
-  twiml.message('Thanks for the text');
+app.post('/', function (req, res) {
+  var body = '';
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+  req.on('data', function (data) {
+    body += data;
+  });
+
+  req.on('end', function () {
+    var data = qs.parse(body);
+    var twiml = new twilio.TwimlResponse();
+    var jsonString = JSON.stringify(data);
+    var jsonDataObject = JSON.parse(jsonString);
+    var employeeData = getEmployeesFromAPI(jsonDataObject.Body, function(err, data){
+      
+      twiml.message("Data: " + data);
+      res.writeHead(200, {'Content-Type': 'text/xml'});
+      res.end(twiml.toString());
+    });
+
+  });
+  //
+  //
+  //  fs.writeFileSync('consolelog.txt', util.inspect(req));
+  //  var twiml = new twilio.TwimlResponse();
+  //  twiml.message('Thanks for the text');
+  //
+  //  res.writeHead(200, {'Content-Type': 'text/xml'});
+  //  res.end(twiml.toString());
 
 });
 /*
@@ -58,11 +84,13 @@ function getEmployeesFromDB(firstName, res){
   });
 };
 */
-function getEmployeesFromAPI(firstName, res){
+function getEmployeesFromAPI(firstName, callback) {
   var url = "http://employeesapi.mybluemix.net/api/Employees?filter%5Bwhere%5D%5Bfirst_name%5D=" + firstName;
   console.log("url", url);
   client.get(url, function (data, response) {
-    res.json(data);
+    //    res.json(data);
+    console.log("the data is: " + prettyjson.render(data));
+    callback(null, prettyjson.render(data));
   });
 
 };
